053f485be279ece82eb39068c1e73e53053598ac
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-10-06 17:27:04 -0500

    Renombra Main como App y app como main

--- js/Main.js
+++ js/Main.js
@@ -1,72 +1,14 @@
-const Main = Vue.component('main', {
-  data() {
-    return {
-      hello: 'Hello Vue!',
-      drawer: false,
-    };
-  },
-  computed: {
-    ...Vuex.mapState(['counter'])
-  },
-  template: `<div>
-    <v-app>
-      <v-navigation-drawer app v-model="drawer">
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title">
-              Vue Simple App
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Menu
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+import router from './router.js';
+import store from './store.js';
 
-        <v-divider></v-divider>
+import App from './App.js';
 
-        <v-list dense nav>
-          <v-list-item router to="/">
-            <v-list-item-icon>
-              <v-icon>home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item router to="/about">
-            <v-list-item-icon>
-              <v-icon>info</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>About</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+// common
+import Counter from './Counter.js';
 
-      <v-app-bar app color="light-blue darken-1" dark>
-        <v-app-bar-nav-icon @click.stop="drawer=!drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>HTML5 + Vue + Router + Vuex + Vuetify</v-toolbar-title>
-      </v-app-bar>
-
-      <v-content>
-        <v-card>
-          <v-card-text>
-            <p>{{ hello }}</p>
-
-            <v-badge>
-              <template v-slot:badge>{{ counter }}</template>
-              <v-icon>alarm_add</v-icon>
-            </v-badge>
-          </v-card-text>
-        </v-card>
-
-        <v-container fluid>
-          <router-view></router-view>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>`
-});
-
-export default Main;
+var app = new Vue({
+  router,
+  store,
+  vuetify: new Vuetify(),
+  render: h => h(App),
+}).$mount('#app');
--- js/app.js
+++ js/app.js
@@ -1,14 +1,73 @@
-import router from './router.js';
-import store from './store.js';
+const App = {
+  name: 'app',
+  data() {
+    return {
+      hello: 'Hello Vue!',
+      drawer: false,
+    };
+  },
+  computed: {
+    ...Vuex.mapState(['counter'])
+  },
+  template: `<div>
+    <v-app>
+      <v-navigation-drawer app v-model="drawer">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Vue Simple App
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Menu
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-import Main from './Main.js';
+        <v-divider></v-divider>
 
-// common
-import Counter from './Counter.js';
+        <v-list dense nav>
+          <v-list-item router to="/">
+            <v-list-item-icon>
+              <v-icon>home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item router to="/about">
+            <v-list-item-icon>
+              <v-icon>info</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-var app = new Vue({
-  router,
-  store,
-  vuetify: new Vuetify(),
-  render: h => h(Main),
-}).$mount('#app');
+      <v-app-bar app color="light-blue darken-1" dark>
+        <v-app-bar-nav-icon @click.stop="drawer=!drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>HTML5 + Vue + Router + Vuex + Vuetify</v-toolbar-title>
+      </v-app-bar>
+
+      <v-content>
+        <v-card>
+          <v-card-text>
+            <p>{{ hello }}</p>
+
+            <v-badge>
+              <template v-slot:badge>{{ counter }}</template>
+              <v-icon>alarm_add</v-icon>
+            </v-badge>
+          </v-card-text>
+        </v-card>
+
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>`
+};
+
+export default App;
