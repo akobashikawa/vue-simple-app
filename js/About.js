53ba4f96df303524b142579d2b0e003b3106f267
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-10-12 21:04:04 -0500

    Presenta counter dentro de un dialogo

--- js/About.js
+++ js/About.js
@@ -5,7 +5,8 @@ const About = {
   components: { Counter },
   data() {
     return {
-      hello: 'Hello About!'
+      hello: 'Hello About!',
+      showCounter: false
     };
   },
   template: `<div>
@@ -13,9 +14,21 @@ const About = {
         <v-card-title>About</v-card-title>
         <v-card-text>
           <p>{{ hello }}</p>
-          <counter></counter>
+
+          <v-btn @click="showCounter=true">Counter</v-btn>
+          
         </v-card-text>
       </v-card>
+
+      <v-dialog persistent v-model="showCounter">
+        <v-card>
+          <counter></counter>
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn text @click="showCounter=false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>`
 };
 
